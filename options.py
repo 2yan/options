1ea72b69aa009d7f593b5d6b0b3d5e57dff86d2f
Ryan Francis <rfrancis@graphicproducts.local>
2017-06-30 14:55:42 -0700

    changes

--- options.py
+++ options.py
@@ -1,6 +1,7 @@
 from ryan_tools import *
 import pandas_datareader.data as web
 from pandas_datareader.data import Options
+import fuckit
 
 def download(ticker, stock_prices = False):
     data = Options(ticker, 'yahoo').get_all_data()
@@ -13,9 +14,10 @@ def download(ticker, stock_prices = False):
     
     data.columns = data.columns.str.lower()
     data['days_left'] = ( data['expiry'] - datetime.datetime.today()).dt.days
-    data['lasttradedate'] = data['lasttradedate'].apply(datetime.datetime.fromtimestamp )
+    #data['lasttradedate'] = data['lasttradedate'].apply(datetime.datetime.fromtimestamp )
     if stock_prices:
         data = get_stock_prices(data, ticker)
+    data.index = data['strike']
     return data
 
 
@@ -47,6 +49,48 @@ def get_project_value(data):
     
     
 
-data = download('aapl', True)
-data['value'] = get_project_value(data)
-data['extra_value'] = data['lastprice'] - data['value']
+def find_point(data):
+
+    for index in data.index:
+
+        bid = data.loc[index, 'bid']
+        ask = data.loc[index, 'ask']
+        
+        with fuckit:
+            data.loc[index, 'profit 0.5'] = bid -  data.loc[index - 0.5, 'ask']
+
+        with fuckit:
+            data.loc[index, 'profit 1'] = bid -  data.loc[index - 1, 'ask']
+            
+        with fuckit:
+            data.loc[index, 'profit 2'] = bid -  data.loc[index - 1.5, 'ask']
+            
+        with fuckit:
+            data.loc[index, 'profit 2'] = bid -  data.loc[index - 2, 'ask']
+
+    return data
+
+def timevalue(data, start, end):
+    data = data[(data['days_left'] == start) | (data['days_left'] == end)]
+    data = data[data['lasttradedate'].apply(lambda x: read_date(get_date_str(x))) == read_date(get_date_str(datetime.datetime.today()))]
+    
+    data['unique'] = data['strike'].apply(str) + data['type'].apply(str)
+    data = data[data['unique'].duplicated( keep = False)]
+    puts = data[data['type'] == 'put']
+    calls = data[data['type'] == 'call']
+    print('ALL')
+    print(data.groupby('days_left')['lastprice'].mean())
+
+    print('CALLS')
+    print(calls.groupby('days_left')['lastprice'].mean())
+
+    print('PUTS')
+    print(puts.groupby('days_left')['lastprice'].mean())
+    return data
+    
+def the_spot(data):
+    data.plot('strike', 'bid', kind = 'scatter')
+data = download('spy')
+
+
+
